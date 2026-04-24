b5066357c3e99c73df31027390b4a3f0b9ea3193
Andrés Felipe Jaimes Martínez <andresfelipeja51@gmail.com>
2026-04-24 09:37:59 +0100

    Actualizado con javadocs

--- Cono.java
+++ Cono.java
@@ -1,34 +1,79 @@
+/**
+ * The type Cono.
+ */
 public class Cono {
     private double radioBase;
     private double altura;
 
+    /**
+     * Instantiates a new Cono.
+     */
     public Cono() {}
 
+    /**
+     * Instantiates a new Cono.
+     *
+     * @param radioBase the radio base
+     * @param altura    the altura
+     */
     public Cono(double radioBase, double altura) {
         this.radioBase = radioBase;
         this.altura = altura;
     }
 
+    /**
+     * Gets radio base.
+     *
+     * @return the radio base
+     */
     public double getRadioBase() {
         return radioBase;
     }
 
+    /**
+     * Sets radio base.
+     *
+     * @param radioBase the radio base
+     */
     public void setRadioBase(double radioBase) {
         this.radioBase = radioBase;
     }
 
+    /**
+     * Gets altura.
+     *
+     * @return the altura
+     */
     public double getAltura() {
         return altura;
     }
 
+    /**
+     * Sets altura.
+     *
+     * @param altura the altura
+     */
     public void setAltura(double altura) {
         this.altura = altura;
     }
 
+    /**
+     * Calcular area base double.
+     *
+     * @param radioBase the radio base
+     * @return the double
+     */
     public double calcularAreaBase(double radioBase) {
         return Math.PI * radioBase * radioBase;
     }
 
+    /**
+     * Calcular volumen double.
+     *
+     * @param radioBase the radio base
+     * @param altura    the altura
+     * @return the double
+     */
     public double calcularVolumen(double radioBase, double altura) {
         return (Math.PI * radioBase * radioBase * altura) / 3;
     }
--- Main.java
+++ Main.java
@@ -1,4 +1,12 @@
+/**
+ * The type Main.
+ */
 public class Main {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
 
         Cono cono1 = new Cono(3,4);
